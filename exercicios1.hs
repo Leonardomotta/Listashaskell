e8967c4dc308aa2cd284fe8309ab9f4862946a4f
Leonardomotta <leonardo.mota@ccc.ufcg.edu>
2018-10-06 09:11:49 -0300

    lista 1 completa

--- exercicios1.hs
+++ exercicios1.hs
@@ -9,7 +9,10 @@ square x = x*x
 {-
 - Implemente a funcao potencia, que retorna o resultado de x elevado a y 
 -}
-pow x y = x**y
+pow x 0 = 1
+pow x y 
+    | y > 0 = x*(pow x(y-1))
+    |otherwise = 1/(pow x(-y))
 
 
 {-
@@ -35,20 +38,30 @@ fib x = fib(x-1) + fib(x-2)
 {-
 - Calcula um MDC de dois numeros usando o algoritmo de Euclides. 
 -}
-mdc x y = undefined
+mdc 0 y = y
+mdc x 0 = x
+mdc x y = mdc y r 
+    where
+        r = mod x y
 
 {-
 - Calcula um MMC de dois numeros. 
 -}
-mmc x y = undefined
+divisor x y n = (mod  n x == 0)  && (mod n  y== 0)
+mmc x y = head ys
+    where ys =  filter (divisor x y) [(min x y)..x*y]
+
 
 {-
 - Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
 - o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
 -}
-coprimo x y = undefined
+coprimo x y = mdc x y == 1
 
 {-
-- Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
+- Calcula a conjectura de Goldbach, 
+que diz que um numero par maior 
+que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
 -}
-goldbach x = undefined
+goldbach x = [(z,y)|z <- ys, y <- ys,z+y==x]
+        where ys = filter isPrime [1..x]
