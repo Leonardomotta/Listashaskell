7ca3c7b6a863001a1ee3acbbc9f4c6768310a384
Leonardomotta <leonardo.mota@ccc.ufcg.edu>
2018-10-07 12:40:33 -0300

    Lista 4 concluida

--- exercicios4.hs
+++ exercicios4.hs
@@ -56,16 +56,66 @@ meuReverso = \xs -> case xs of [] -> []
 isPalindrome :: Eq a=> [a] -> Bool
 isPalindrome = \xs -> xs == meuReverso  xs
 
+conjunto z [] = z
+conjunto z x 
+             | elem (head x) z  = conjunto z (tail x)
+             | otherwise = conjunto (z ++ [head x]) (tail x)
 
-compress = undefined
-compact xs = undefined
-encode xs = undefined
-split xs i = undefined
-slice xs imin imax = undefined
-insertAt el pos xs = undefined
-sort xs = undefined
-mySum xs = undefined
-maxList xs = undefined
-buildPalindrome xs = undefined
-mean xs = undefined
-myAppend xs ys = undefined
+compress :: Eq a => [a] -> [a]
+compress = \xs ->  (conjunto [head xs] (tail xs))
+
+removeAll :: Eq a => [a]-> a -> [a]
+removeAll = \xs a -> if (xs == []) then []
+                    else if (head xs ) == a then [] ++ (removeAll (tail xs) a)
+                    else  ([head xs]) ++ (removeAll (tail xs) a)
+
+compact :: Eq a => [a] -> [a]
+compact = \xs -> if (xs == []) then []
+                 else [k|k <- xs,k==(head xs)] ++ (compact (removeAll xs (head xs)))
+
+encode :: Eq a => [a] -> [(a,Int)]
+encode = \xs ->if (xs == []) then []
+              else [(head (a xs) , length (a xs) )] ++ (encode(removeAll xs (head xs)))
+              
+a :: Eq a => [a]->[a]
+a = \xs -> [k|k <- xs, k == (head xs)]
+
+split::Eq a => [a]->Int->[[a]]
+split = \xs i -> [take i xs , drop i xs]
+
+
+slice xs imin imax = [x|x <-xs , (not)(x `elem`((take (imin-1)xs) ++ (drop (imax) xs) ) )    ]
+
+
+insertAt = \el pos xs -> ((take (pos-1) xs) ++ [el] ++ (drop (pos-1) xs))
+
+minList [x] = x
+minList (x:xs) = if (x < (minList xs)) then x else minList xs
+
+remove e (x:xs) | e == x = xs
+                | otherwise = x:(remove e xs)
+sort [] = []
+sort xs = x:ys 
+    where
+        x = minList xs
+        ys = sort (remove x xs) 
+
+
+mySum = \xs -> if length xs == 0 then 0
+               else (head xs) + (mySum  (tail xs)) 
+
+m :: Int -> Int -> Int
+m = \a b -> if a > b then a else b              
+
+maxList = \xs -> if length (tail xs) == 0 then head xs
+                 else (m (head xs) (maxList (tail xs)))
+
+buildPalindrome =  \xs -> xs ++ help xs
+
+help = \xs -> if length xs == 0 then []
+              else ([last xs]) ++ (help (init xs))
+
+mean = \xs -> (mySum xs ) `div` (meuLength xs)
+
+myAppend :: [a] -> [a] -> [a]
+myAppend = \xs ys -> (foldr(:)  ys xs)  
